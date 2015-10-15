01d456e528b00c460c0420c94191da9165589602
yeonsik <bbangsik4508@naver.com>
2015-10-15 14:25:22 +0900

    category

---
+++ picView_v0/webcontent/jsp/category/category.jsp
@@ -0,0 +1,32 @@
+<%@ page language="java" contentType="text/html; charset=EUC-KR"
+    pageEncoding="EUC-KR"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=EUC-KR">
+<script type="text/javascript" src="./js/jquery-1.10.2.min.js"></script>
+<script type="text/javascript" src="./js/main_menu.js"></script>
+<script type="text/javascript" src="./js/notipopup.js"></script>
+<script src="./js/siteSecurity.js"></script>
+<script type="text/javascript" src="./js/jquery.scrollUp.min.js"></script>
+<link rel="stylesheet" type="text/css" href="./css/main_menu.css" />
+
+<title>Insert title here</title>
+</head>
+<body>
+	<nav id="aside_menu">
+         <ul class="left-menu1">
+            <li><img src="./imgs/left_tit.jpg" alt=""><li>
+            <li><a href="#">Brand Story</a></li>
+            <li><a href="#">Store Info</a></li>
+            <li><a href="#">News&amp;Note</a></li>
+            <li><a href="#">Eye wear</a></li>
+         </ul>
+      
+      <ul class="left-menu2">
+         <li><img src="./imgs/kamaeng.jpg" alt=""></li>
+      </ul>
+      <a href="#" id="menubutton"><img src="./imgs/menu_button2.PNG" alt=""></a>
+   </nav>
+</body>
+</html>
---
+++ picView_v0/webcontent/jsp/category/js/main_menu.js
@@ -0,0 +1,103 @@
+$(function() {
+	$('#notipopup').notipopup({
+		startX : '20%',
+		startY : '10%',
+		close : '.close',
+		todayclose : '.todayclose'
+	});
+})
+
+$(function() {
+		$.scrollUp({
+			scrollName : 'scrollUp', // Element ID
+			topDistance : '300', // Distance from top before showing element
+			// (px)
+			topSpeed : 300, // Speed back to top (ms)
+			animation : 'fade', // Fade, slide, none
+			animationInSpeed : 200, // Animation in speed (ms)
+			animationOutSpeed : 200, // Animation out speed (ms)
+			scrollText : 'Scroll to top', // Text for element
+			activeOverlay : false, // Set CSS color to display scrollUp active
+		// point, e.g '#00FFFF'
+		});
+	});
+
+$(function() {
+		$('#aside_menu').css({
+			left : '-200px'
+		}).addClass('open');
+		$('body').css({
+			marginLeft : '-200px'
+		});
+		$('#menubutton').find('img').attr('src', './imgs/menu_button1.PNG');
+	});
+
+$(function() {
+	$('.product').on('click', function(e) {
+		$('#content').empty().load("New.html");
+		e.preventDefault();
+	});
+
+});
+
+$(function() {
+	$('body').siteSecurity({
+		f12 : 'n',
+		rightclick : 'y',
+		select : 'y',
+		drag : 'y'
+	});
+})
+
+$(function() {
+	$('#content').load("main.html");
+
+	$('.logo > a').on('click', function(e) {
+		$('#content').empty().load("main.html");
+		e.preventDefault();
+	});
+
+	$('#about_glass').on('click', function(e) {
+		$('#content').empty().load("glassbox_About.html");
+		e.preventDefault();
+	});
+
+	$('.side_menu ul').addClass('sub_display');
+
+	$('.right-menu a').on('click', function() {
+		var href = $(this).attr('href');
+		$('.side_menu ul').each(function() {
+			var id = $(this).attr('id');
+			if (href == id) {
+				$(this).removeClass('sub_display');
+			} else {
+				$(this).addClass('sub_display')
+			}
+		});
+		//$(this).addClass('sub_display');
+
+		return false;
+	});
+
+	$('#menubutton').on('click', function() {
+		$('#aside_menu').toggleClass('open');
+		if ($('#aside_menu').hasClass('open')) {
+			$('#aside_menu').animate({
+				left : '-200px'
+			}, 300);
+			$('body').animate({
+				marginLeft : '-200px'
+			}, 300);
+			$(this).find('img').attr('src', './imgs/menu_button1.PNG');
+		} else {
+			$('#aside_menu').animate({
+				left : '0px'
+			}, 300);
+			$('body').animate({
+				marginLeft : '0px'
+			}, 300);
+			$(this).find('img').attr('src', './imgs/menu_button2.PNG');
+		}
+		return false;
+	});
+})
